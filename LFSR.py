21033896022f6ef3bea9433d6d1a42c0c7e95642
vegkva <81537577+vegkva@users.noreply.github.com>
2022-02-06 18:39:53 +0100

    Update LFSR.py

--- LFSR.py
+++ LFSR.py
@@ -1,15 +1,52 @@
 
+# "this is a test" in binary
 plaintext = [0,1,1,1,0,1,0,0,0,1,1,0,1,0,0,0,0,1,1,0,1,0,0,1,0,1,1,1,0,0,1,1,0,0,1,0,0,0,0,0,0,1,1,0,1,0,0,1,0,1,1,1,0,0,1,1,
              0,0,1,0,0,0,0,0,0,1,1,0,0,0,0,1,0,0,1,0,0,0,0,0,0,1,1,1,0,1,0,0,0,1,1,0,0,1,0,1,0,1,1,1,0,0,1,1,0,1,1,1,0,1,0,0]
 
 """
-With this inital state, the cycle will
+With this initial state, the cycle will
 repeat after 254 cycles.
 This means one can create a key with length
 254 without repeating states
 """
 feedback = [0,1,1,0,1,0,0,1]
 initial_state = [1,0,0,1,0,1,0,0]
+
+
+"""
+Simple implementation of the LFSR
+Can only XOR 4 digits
+"""
+def lfsr(feedback, state, k):
+    new_first_pos = 0
+    xor_index = indices(feedback, 1)
+
+    next_state = initial_state.copy()
+    output = []
+    count = 0
+    #print("initial: ", next_state)
+    for i in range(k):
+        if (len(xor_index) == 2):
+            new_first_pos = ((next_state[xor_index[0]] + next_state[xor_index[1]]) % 2)
+            #print("xor'd index", xor_index[0], "and", xor_index[1], "of", next_state, " and got:", new_first_pos)
+        if (len(xor_index) == 3):
+            new_first_pos = ((next_state[xor_index[0]] + next_state[xor_index[1]] + next_state[xor_index[2]]) % 2)
+            #print("xor'd index", xor_index[0], "and", xor_index[1], "and", xor_index[2], "of", next_state, " and got:", new_first_pos)
+        if (len(xor_index) == 4):
+            new_first_pos = ((next_state[xor_index[0]] + next_state[xor_index[1]] + next_state[xor_index[2]] + next_state[xor_index[3]]) % 2)
+            #print("xor'd index", xor_index[0], "and", xor_index[1], "and", xor_index[2], "and", xor_index[3], "of", next_state, " and got:", new_first_pos)
+        output.append(next_state.pop())
+        #print("popped ", output[-1], " to output")
+        #print("state before new first pos: ", next_state)
+        next_state.insert(0, new_first_pos)
+        #print("next state completed with new first pos: ", next_state)
+
+        # Check if initial state repeats
+        if state == next_state:
+            print("cycle repeated after ", count, "steps")
+        count += 1
+
+    return output
 
 
 """
@@ -58,47 +95,11 @@ def indices(lst, item):
     return [i for i, x in enumerate(lst) if x == item]
 
 
-"""
-Simple implementation of the LFSR
-Can only XOR 4 digits
-"""
-def lfsr(feedback, state, k):
-    new_first_pos = 0
-    xor_index = indices(feedback, 1)
 
-    next_state = initial_state.copy()
-    output = []
-    count = 0
-    #print("initial: ", next_state)
-    for i in range(k):
-        if (len(xor_index) == 2):
-            new_first_pos = ((next_state[xor_index[0]] + next_state[xor_index[1]]) % 2)
-            #print("xor'd index", xor_index[0], "and", xor_index[1], "of", next_state, " and got:", new_first_pos)
-        if (len(xor_index) == 3):
-            new_first_pos = ((next_state[xor_index[0]] + next_state[xor_index[1]] + next_state[xor_index[2]]) % 2)
-            #print("xor'd index", xor_index[0], "and", xor_index[1], "and", xor_index[2], "of", next_state, " and got:", new_first_pos)
-        if (len(xor_index) == 4):
-            new_first_pos = ((next_state[xor_index[0]] + next_state[xor_index[1]] + next_state[xor_index[2]] + next_state[xor_index[3]]) % 2)
-            #print("xor'd index", xor_index[0], "and", xor_index[1], "and", xor_index[2], "and", xor_index[3], "of", next_state, " and got:", new_first_pos)
-        output.append(next_state.pop())
-        #print("popped ", output[-1], " to output")
-        #print("state before new first pos: ", next_state)
-        next_state.insert(0, new_first_pos)
-        #print("next state completed with new first pos: ", next_state)
-
-        # Check if initial state repeats
-        if state == next_state:
-            print("cycle repeated after ", count, "steps")
-        count += 1
-
-    return output
 
 
 
 key = lfsr(feedback, initial_state, 112)
 
-
-ciphertext = encrypt(plaintext, key, 8)
-print("encrypted: ",ciphertext)
-
+print("encrypted: ", encrypt(plaintext, key, 8))
 print("decrypted: ", decrypt(ciphertext, key, 8))
